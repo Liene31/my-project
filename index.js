43ffbaf2766d28cf9a23e98eba5de0e5251cbd5e
Liene31 <67922137+Liene31@users.noreply.github.com>
2025-09-03 11:56:47 +0200

    Refactor layout and remove index.html
    Removed index.html and updated navigation across pages to exclude the Home link. Improved progress bar styling and structure in about.html and style.css. Updated image sizes and inventory items. Enhanced responsive design and layout for journey.html and wisdom.html. Modified wisdom quotations in index.js.

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const wisdomArray = [
   "Code is like humor. When you have to explain it, it's bad.",
   "Simplicity is the soul of good software.",
   "Don't optimize prematurely; clarity comes first.",
-  "Every great developer you know got there by solving problems they didn't know how to solve before.",
-  "Debugging is twice as hard as writing the code in the first place; if you write the code as cleverly as possible, you're not smart enough to debug it.",
+  "First, solve the problem. Then, write the code.",
+  "Coding like poetry should be short and concise.",
   "Code is a spell; clarity is its true component.",
   "Patience is mana; spend it wisely.",
 ];
